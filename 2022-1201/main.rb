ae0da7e2a101aec58efae98fc78d9de1f5e574ca
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-01 23:10:46 +1300

    still having buggy code but trying somethings different

--- 2022-1201/main.rb
+++ 2022-1201/main.rb
@@ -11,8 +11,31 @@ class Main
   end
 
   def run
-    max_calories = 0
-    elf_with_max = nil
+    #first_elf = nil
+    #last_elf = nil
+    elves = {}
+    # elves = {
+    #   12: {
+    #     calories: 123456,
+    #   },
+    #   33: {
+    #     calories: 123456,
+    #   }
+    # }
+    positions = {}
+    # positions = {
+    #   1: 12,
+    #   2: 33,
+    #   3: 28,
+    # }
+    gold_elf = nil
+    gold_calories = 0
+
+    silver_elf = nil
+    silver_calories = 0
+
+    bronze_elf = nil
+    bronze_calories = 0
 
     elf = 1
     elf_total = 0
@@ -22,6 +45,27 @@ class Main
       #puts "elf[" + elf.to_s + "] food[" + index.to_s + "]=" + item.to_s
       #puts "isnumber? " + is_number?(item).to_s
       if is_number?(item) == false
+
+        if elf_total > gold_calories
+          # bump the others down
+          bronze_elf = silver_elf
+          bronze_calories = silver_calories
+          silver_elf = gold_elf
+          silver_calories = gold_calories
+          gold_elf = elf
+          gold_calories = elf_total
+        elsif elf_total > silver_calories
+          # bump the others down
+          bronze_elf = silver_elf
+          bronze_calories = silver_calories
+          silver_elf = elf
+          silver_calories = elf_total
+        elsif elf_total > bronze_calories
+          bronze_elf = elf
+          bronze_calories = elf_total
+        end
+        # might be able to fix with linked lists sorting
+
         elf += 1
         elf_total = 0 
          #unless still at one
@@ -31,15 +75,14 @@ class Main
 
       elf_total += item.to_i
 
-      if elf_total > max_calories
-        elf_with_max = elf
-        max_calories = elf_total
-      end
-
       index += 1
     end
     
-    max_calories
+    #gold_calories
+    puts "elf[" + gold_elf.to_s + "] calories=" + gold_calories.to_s
+    puts "elf[" + silver_elf.to_s + "] calories=" + silver_calories.to_s
+    puts "elf[" + bronze_elf.to_s + "] calories=" + bronze_calories.to_s
+    top3_total = gold_calories + silver_calories + bronze_calories
   end
 end
 
@@ -47,7 +90,10 @@ end
 #file_data = file.read.split("\n")
 #puts file_data.class.name
 
+main = Main.new("input-00.txt")
+result = main.run
+puts "result = "+ result.to_s # should be 24,000
+
 main = Main.new("input.txt")
 result = main.run
-
-puts "result = ", result
+puts "result = " + result.to_s # should be 72,478
