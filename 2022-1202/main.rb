03c5edb442906579ebcab5eceb27cb5cbd7f7775
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-02 22:45:47 +1300

    have function working to check outcome

--- 2022-1202/main.rb
+++ 2022-1202/main.rb
@@ -17,10 +17,13 @@ class Main
     PAPER.to_sym => 2,
     SCISSORS.to_sym => 3,
   }
-  OUTCOME = {
-    WIN: 6,
-    LOSE: 0,
-    DRAW: 3,
+  WIN = "WIN"
+  LOSE = "LOSE"
+  DRAW = "DRAW"
+  OUTCOME_POINTS = {
+    WIN.to_sym => 6,
+    LOSE.to_sym => 0,
+    DRAW.to_sym => 3,
   }
 
   def initialize(filename)
@@ -29,20 +32,49 @@ class Main
   end
 
   def run
+    my_total = 0
     index = 0
     while index < @data.count
       round = @data[index]
       index +=1
+      my_points = 0
 
       choices = round.split(" ")
       them_enc = choices[0]
       me_enc = choices[1]
       them = THEM[them_enc.to_sym]
       me = MINE[me_enc.to_sym]
-      puts "choices: them[#{them}] me[#{me}]"
+
+      my_points = POINTS[me.to_sym]
+      puts "choices: them[#{them}] me[#{me}+#{my_points}]"
+      #puts "mypoints = " + my_points.to_s
+
+      # TODO: check who wins
+      outcome = get_outcome(them, me)
+      puts "outcome: #{outcome}"
+      # then add points
+
+      my_total += my_points
     end
   end
 
+  def get_outcome(their_choice, my_choice)
+    if (their_choice == my_choice)
+      return DRAW
+    end
+
+    # rock < paper
+    # paper < scissors
+    # scissors < rock
+    rock_to_paper = (their_choice == ROCK && my_choice == PAPER)
+    paper_to_scissors = (their_choice == PAPER && my_choice == SCISSORS)
+    scissors_to_rock = (their_choice == SCISSORS && my_choice == ROCK)
+    if (rock_to_paper || paper_to_scissors || scissors_to_rock) 
+      return WIN
+    else
+      return LOSE
+    end
+  end
 end
 
 main = Main.new("input2a.txt")
