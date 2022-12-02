12c8cc45d7fad527e517818fae23b7daecae5e66
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-02 23:33:20 +1300

    started idea of automating the folder creation

---
+++ aoc-setup.sh
@@ -0,0 +1,12 @@
+# // create .gitignore with Session.vim if not already created
+# // create README.md at root if not already created
+
+# // take in year form command line argument
+# // loop through from the 1st to the 25th
+    # // create the folder if not already created
+        # // create inputDDa.txt - ex: input01a.txt
+        # // create mainDDa.rb - ex: main01a.rb
+        # // create readmeDD.md - ex: readme01.md
+
+# // START: create just a simple method to create the folder for the day
+
