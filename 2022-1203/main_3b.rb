5b0e65ce0992fe03f5fcf2edc81726948217b0a1
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-04 09:43:41 +1300

    have results working as expected

--- 2022-1203/main_3b.rb
+++ 2022-1203/main_3b.rb
@@ -10,14 +10,13 @@ class Main
     priority_map = letters.each_with_index.map {|x, i| [x.to_sym, i+1]}.to_h
   end
   
-  def get_intersect(item)
-      chars = item.split('')
-      half = chars.length / 2
-      part1 = chars[0..(half - 1)]
-      part2 = chars[half..(chars.length - 1)]
+  def get_intersect(group)
+      part1 = group[0].split('')
+      part2 = group[1].split('')
+      part3 = group[2].split('')
 
       #puts part1.join+ "[#{part1.length.to_s}]" + part2.join + "[#{part2.length.to_s}]"
-      intersect = (part1 & part2).first 
+      intersect = ((part1 & part2) & part3).first 
   end
 
   def run
@@ -31,9 +30,9 @@ class Main
       group << item
 
       if group.length == 3 
-        puts "group: " + group.to_s
-        #intersect = get_intersect(group)
-        #priorities << @priority_map[intersect.to_sym]
+        intersect = get_intersect(group)
+        puts "group: " + group.to_s + " >> INT[#{intersect.to_s}]"
+        priorities << @priority_map[intersect.to_sym]
         group = [] # reset group before going to next group
       end
     end
