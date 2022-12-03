de5fe6b542b3e7ed3dfcd5c452d2f8a24268f9a6
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-04 09:22:54 +1300

    refactored to make more readable

--- 2022-1203/main_3a.rb
+++ 2022-1203/main_3a.rb
@@ -9,6 +9,16 @@ class Main
     letters = ('a'..'z').to_a + ('A'..'Z').to_a
     priority_map = letters.each_with_index.map {|x, i| [x.to_sym, i+1]}.to_h
   end
+  
+  def get_intersect(item)
+      chars = item.split('')
+      half = chars.length / 2
+      part1 = chars[0..(half - 1)]
+      part2 = chars[half..(chars.length - 1)]
+
+      #puts part1.join+ "[#{part1.length.to_s}]" + part2.join + "[#{part2.length.to_s}]"
+      intersect = (part1 & part2).first 
+  end
 
   def run
     result = nil
@@ -18,14 +28,8 @@ class Main
       item = @data[index]
       index += 1
 
-      chars = item.split('')
-      half = chars.length / 2
-      part1 = chars[0..(half - 1)]
-      part2 = chars[half..(chars.length - 1)]
+      intersect = get_intersect(item)
 
-      #puts part1.join+ "[#{part1.length.to_s}]" + part2.join + "[#{part2.length.to_s}]"
-      intersect = (part1 & part2).first 
-      #puts intersect
       priorities << @priority_map[intersect.to_sym]
     end
     #puts "priorities" + priorities.to_s
