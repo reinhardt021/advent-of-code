0aa004686146789917846ec16eb52064dc1f74f5
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-05 09:35:05 +1300

    tried a solution but already hit bottleneck

--- 2022-1204/main_4a.rb
+++ 2022-1204/main_4a.rb
@@ -15,11 +15,30 @@ class Main
       #your actual list might contain larger numbers. 
 
       puts "part1: #{parts[0].to_s} && part2: #{parts[1].to_s}"
+      # TODO: check how if pair fully contains another
+      result = is_range_within_another(parts[0], parts[1])
     end
 
     # In how many assignment pairs 
     # does one range fully contain the other?
 
+  end
+
+  def is_range_within_another(range1, range2)
+    range_with_greater_min = nil
+    # check if min of one is greater than other
+    min_max1 = range1.split('-')
+    min_max2 = range2.split('-')
+    # tricky situation if equal
+    # might just check one with smaller magnitude
+    # if smaller magnitude has min within larger and max within larger
+    # then you have a true
+    if (min_max1 < min_max2)
+      range_with_greater_min = 
+    else
+    end
+    # then check if the one with greater min has smaller max
+    
   end
 
 end
