30ff634496edc621127e2d67245b70cc96b80987
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-05 09:54:04 +1300

    have condition working

--- 2022-1204/main_4a.rb
+++ 2022-1204/main_4a.rb
@@ -24,30 +24,29 @@ class Main
   end
 
   def is_range_within_another(range1, range2)
-    # check if min of one is greater than other
     min_max1 = range1.split('-')
     min_max2 = range2.split('-')
     magnitude1 = min_max1[1].to_i - min_max1[0].to_i
     magnitude2 = min_max2[1].to_i - min_max2[0].to_i
+
     puts "range1: #{range1.to_s}[#{magnitude1}] && range2: #{range2.to_s}[#{magnitude2}]"
     if magnitude1 == magnitude2 
       return min_max1[0] == min_max2[0]
     end
 
-    smaller_range = (magnitude1 < magnitude2) ? min_max1 : min_max2
-    larger_range = (magnitude1 < magnitude2) ? min_max2 : min_max1
+    if magnitude1 < magnitude2
+      smaller_range = min_max1
+      larger_range = min_max2
+    else
+      smaller_range = min_max2
+      larger_range = min_max1
+    end
 
     puts "the smaller range is #{smaller_range.to_s}"
-    # tricky situation if equal
-    # might just check one with smaller magnitude
     # if smaller magnitude has min within larger and max within larger
-    # then you have a true
-    #if (min_max1 < min_max2)
-      #range_with_greater_min = 
-    #else
-    #end
-    # then check if the one with greater min has smaller max
-    
+    smaller_within_larger_min = larger_range[0] <= smaller_range[0]
+    smaller_within_larger_max = smaller_range[1] <= larger_range[1]
+    return smaller_within_larger_min && smaller_within_larger_max
   end
 
 end
