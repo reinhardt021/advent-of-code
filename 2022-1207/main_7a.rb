e0e03e83758ac69910e0113ce65a64f9eb5d319a
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-07 21:51:23 +1300

    have the parsing working as I want now to create a tree

--- 2022-1207/main_7a.rb
+++ 2022-1207/main_7a.rb
@@ -1,71 +1,104 @@
 class Main
+  CMD = '$'
+  CD = 'cd'
+  UP = '..'
+  LS = 'ls'
+  DIR = 'dir'
+  FILE = 'file'
+  STORE = 'store'
+  DONE = 'done'
   def initialize(filename)
     file = File.open(filename)
     @data = file.read.split("\n")
   end
   
   def run
-    # - / (dir)
-      #- a (dir)
-        #- e (dir)
-          #- i (file, size=584)
-        #- f (file, size=29116)
-        #- g (file, size=2557)
-        #- h.lst (file, size=62596)
-      #- b.txt (file, size=14848514)
-      #- c.dat (file, size=8504156)
-      #- d (dir)
-        #- j (file, size=4060174)
-        #- d.log (file, size=8033020)
-        #- d.ext (file, size=5626152)
-        #- k (file, size=7214296)
-
     tree = {}
-    # loop through each line to parse
-    # build tree as you go
-    # and build the totals as well as you go
+    # build tree as you go & build the totals as well as you go
     curr_node = nil
     index = 0 
+    store_children = nil #status: nil | 'store' | 'done'
     while index < @data.length
       line = @data[index]
       index += 1
 
       parts = line.split(' ')
-      puts "line: #{parts.to_s}"
-      if parts[0] == '$'
-        # then find part 2 and see what to do next 
+
+      command = nil
+      item = nil
+      part1 = parts[0]
+      puts "part1[#{part1.to_s}].int?[#{part1.is_a?(Integer).to_s}]"
+      if part1 == CMD
         command = parts[1]
-        if command == 'cd'
-          directory = parts[2]
-          if directory == '..'
-            # don't create new node
-            # change curr_node to parent node
-            parent_path = curr_node[:parent]
-            parent_node = tree[parent_path]
-            curr_node = parent_node
-            # then just go to next loop
-          else
-            # check if node exists
-            #   if exists then change to node
-            #   else create a node in the tree
-            puts "create node in tree: #{tree.to_s}"
-            puts "curr_node: #{curr_node.to_s}"
-            parent_path = curr_node ? curr_node[:full_path] : ''
-            suffix = (directory == '/' ? '' : '/')
-            full_path = parent_path + directory + suffix
-            new_node = {
-              parent: parent_path,
-              full_path: full_path, 
-              children: [],
-            }
-            tree[full_path] = new_node
-            curr_node = new_node
-          end
+        store_children = store_children == nil ? nil : DONE
+      elsif part1 == DIR
+        directory = parts[1]
+        parent_path = curr_node ? curr_node[:full_path] : ''
+        #suffix = (directory == '/' ? '' : '/')
+        #full_path = parent_path + directory # + suffix
+        glue = (parent_path == '/' ? '' : '/' )
+        full_path = parent_path + glue + directory
+        item = {
+          type: DIR,
+          full_path: full_path, 
+        }
+        # TODO: store item in children
+      else #(part1.is_a?(Integer))
+        parent_path = curr_node ? curr_node[:full_path] : ''
+        file = parts[1]
+        glue = (parent_path == '/' ? '' : '/' )
+        full_path = parent_path + glue + file
+        item = {
+          type: FILE,
+          full_path: full_path, 
+          size: part1.to_i
+        }
+        # TODO: store item in children
+      end
+
+      if command == CD
+        puts "TREE: #{tree.to_s}"
+        puts "curr_node: #{curr_node.to_s}"
+        puts "    LINE: #{parts.to_s}"
+        directory = parts[2]
+        if directory == UP
+          # don't create new node
+          # change curr_node to parent node
+          parent_path = curr_node[:parent]
+          parent_node = tree[parent_path]
+          curr_node = parent_node
+          # then just go to next loop
+        else
+          # check if node exists 
+          #   if exists then change to node - stretch?
+          #   else create a node in the tree
+          parent_path = curr_node ? curr_node[:full_path] : ''
+          #suffix = (directory == '/' ? '' : '/')
+          glue = (directory == '/' || parent_path == '/') ? '' : '/'
+          full_path = parent_path + glue + directory #+ suffix
+          new_node = {
+            type: 'dir',
+            parent: parent_path,
+            full_path: full_path, 
+            children: [],
+            size: 0
+          }
+          tree[full_path] = new_node
+          curr_node = new_node
         end
-        if command == 'ls'
-          # next few lines will be within the 
-          puts 'start storing after this'
-        end
+        puts "TREE: #{tree.to_s}"
+        puts "curr_node: #{curr_node.to_s}]\n\n"
+      end
+
+      if command == LS
+        # next few lines will be within the 
+        puts 'start storing after this'
+        store_children = STORE
+      end
+
+
+      if store_children == STORE
+        curr_node[:children] << item if item != nil
       end
       # parse the line
       #   $ means command being run
@@ -85,7 +118,7 @@ class Main
       # can be easier to store more information per node
 
     end
-    puts "this is tree end: #{tree.to_s}"
+    puts "FINAL TREE: #{tree.to_s}"
 
   end
 end
