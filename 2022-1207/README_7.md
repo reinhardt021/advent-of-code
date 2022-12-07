3abe216d88e546269d98e8b955d843743a83c6a2
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-07 23:23:10 +1300

    have new recursive function working as needed now

--- 2022-1207/README_7.md
+++ 2022-1207/README_7.md
@@ -143,4 +143,50 @@ the sum of their total sizes is 95437 (94853 + 584).
 Find all of the directories with a total size of at most 100,000. 
 What is the sum of the total sizes of those directories?
 
+Your puzzle answer was 1648397.
+
+The first half of this puzzle is complete! 
+It provides one gold star: *
+
+
+--- Part Two ---
+
+Now, you're ready to choose a directory to delete.
+
+The total disk space available to the filesystem is 70,000,000. 
+To run the update, you need unused space of at least 30,000,000. 
+You need to find a directory you can delete 
+that will free up enough space to run the update.
+
+In the example above, 
+the total size of the outermost directory 
+(and thus the total amount of used space) 
+is 48,381,165; 
+this means that the size of the unused space 
+must currently be 21,618,835, 
+which isn't quite the 30,000,000 required by the update. 
+Therefore, 
+the update still requires 
+a directory with total size 
+of at least 8,381,165 
+to be deleted before it can run.
+
+To achieve this, you have the following options:
+
+Delete directory e, which would increase unused space by 584.
+Delete directory a, which would increase unused space by 94,853.
+Delete directory d, which would increase unused space by 24,933,642.
+Delete directory /, which would increase unused space by 48,381,165.
+
+Directories e and a are both too small; 
+deleting them would not free up enough space. 
+However, directories d and / are both big enough! 
+Between these, 
+choose the smallest: d, 
+increasing unused space by 24,933,642.
+
+Find the smallest directory that, if deleted, 
+would free up enough space on the filesystem to run the update. 
+What is the total size of that directory?
+
 
---
+++ 2022-1207/main_7b.rb
@@ -0,0 +1,169 @@
+class Main
+  CMD = '$'
+  CD = 'cd'
+  UP = '..'
+  LS = 'ls'
+  DIR = 'dir'
+  FILE = 'file'
+  STORE = 'store'
+  DONE = 'done'
+  def initialize(filename)
+    file = File.open(filename)
+    @data = file.read.split("\n")
+  end
+  
+  def run
+    tree = parse_data(@data)
+    dirs = get_dir_sizes(100000, tree, '/')
+    puts "FINAL TREE: #{tree.to_s}"
+    puts "final DIR within list: #{dirs.to_s}"
+    #sizes = dirs.map { |dir| dir[:size] }
+
+    #return sizes.sum
+  end
+
+  def get_dir_sizes(limit, tree, node_path)
+    dir_sizes = {}
+
+    curr_node = tree[node_path]
+    total_dirs = 0
+    curr_node[:children_dirs].each do |dir_path|
+      # so it does matter we calculate the dir sizes here 1st
+      child_dir_sizes = get_dir_sizes(limit, tree, dir_path)
+      puts 'before dir sizes: ' + dir_sizes.to_s
+      puts 'child dir sizes: ' + child_dir_sizes.to_s
+      dir_sizes = dir_sizes.merge(child_dir_sizes) 
+      puts 'after dir sizes: ' + dir_sizes.to_s
+
+      # once the children calculated then can add to this node size
+      child_node = tree[dir_path]
+      total_dirs += child_node[:size]
+    end
+
+    total_files = curr_node[:children_files].sum
+    total_size = total_files + total_dirs 
+
+    curr_node[:size] = total_size
+    tree[node_path] = curr_node
+
+    dir_sizes[node_path] = total_size
+    #if total_size <= limit
+      #dirs << curr_node
+    #end
+
+    return dir_sizes
+  end
+  
+  def parse_data(lines)
+    # build tree as you go & build the totals as well as you go
+    # can't build totals for directories
+    # because don't know if later lines go into the child directories
+    # will have to just traverse your tree after creation
+    tree = {}
+    curr_node = nil
+    index = 0 
+    store_children = nil #status: nil | 'store' | 'done'
+    while index < lines.length
+      line = lines[index]
+      index += 1
+
+      parts = line.split(' ')
+
+      command = nil
+      item = nil
+      part1 = parts[0]
+      #puts "part1[#{part1.to_s}].int?[#{part1.is_a?(Integer).to_s}]"
+      if part1 == CMD
+        command = parts[1]
+        store_children = store_children == nil ? nil : DONE
+      elsif part1 == DIR
+        directory = parts[1]
+        parent_path = curr_node ? curr_node[:full_path] : ''
+        glue = (parent_path == '/' ? '' : '/' )
+        full_path = parent_path + glue + directory
+        item = {
+          type: DIR,
+          full_path: full_path, 
+        }
+      else 
+        parent_path = curr_node ? curr_node[:full_path] : ''
+        file = parts[1]
+        glue = (parent_path == '/' ? '' : '/' )
+        full_path = parent_path + glue + file
+        item = {
+          type: FILE,
+          full_path: full_path, 
+          size: part1.to_i
+        }
+      end
+
+      if command == CD
+        #puts "TREE: #{tree.to_s}"
+        #puts "curr_node: #{curr_node.to_s}"
+        #puts "    LINE: #{parts.to_s}"
+        directory = parts[2]
+        if directory == UP
+          parent_path = curr_node[:parent]
+          parent_node = tree[parent_path]
+          curr_node = parent_node
+        else
+          # check if node exists 
+          #   if exists then change to node - stretch?
+          #   else create a node in the tree
+          parent_path = curr_node ? curr_node[:full_path] : ''
+          glue = (directory == '/' || parent_path == '/') ? '' : '/'
+          full_path = parent_path + glue + directory #+ suffix
+          new_node = {
+            #type: DIR,
+            parent: parent_path,
+            full_path: full_path, 
+            size: 0,
+            children_dirs: [],
+            children_files: [],
+          }
+          tree[full_path] = new_node
+          curr_node = new_node
+        end
+        #puts "TREE: #{tree.to_s}"
+        #puts "curr_node: #{curr_node.to_s}]\n\n"
+      end
+
+      if command == LS
+        # next few lines will be within the 
+        #puts 'start storing after this'
+        store_children = STORE
+      end
+
+
+      if store_children == STORE && item != nil
+        if item[:type] == DIR
+          curr_node[:children_dirs] << item[:full_path]
+        else
+          curr_node[:children_files] << item[:size]
+        end
+      end
+
+    end
+
+    return tree
+  end
+end
+
+def assertEquals(expected, actual)
+  if expected == actual
+    puts "PASS: exp[#{expected}] == act[#{actual}]"
+  else
+    puts "FAIL: exp[#{expected}] != act[#{actual}]"
+  end
+end
+
+#assertEquals(26, Main.new(in5).run())
+main = Main.new('input_7a.txt')
+results = main.run
+puts "results: " + results.to_s
+assertEquals(24933642, results)
+
+#main = Main.new('input_7b.txt')
+#results = main.run
+#puts "results: " + results.to_s
+
