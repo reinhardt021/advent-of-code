b92bab6e716620071606c504cc5ae6c5edefd2e8
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-11 23:17:46 +1300

    was able to translate from ruby to JS but have some bugs

--- 2022-1211/main_11b.js
+++ 2022-1211/main_11b.js
@@ -25,9 +25,7 @@ function parse_monkeys(lines) {
         index += 1
 
         const parts = line.split(' ').filter(x => x != '');
-        //parts = parts
-        //const parts = line.match(/\S+/)
-        console.log(`parts: ${parts.map(x => `[${x}]`)}`);
+        //console.log(`parts: ${parts.map(x => `[${x}]`)}`);
         const line_type = parts[0]
         if (line_type == MONKEY) {
             curr_monkey = parts[1].match(/\d+/)[0]
@@ -90,6 +88,121 @@ function parse_file(filename) {
     return monkeys;
 }
 
+function display_monkeys(monkeys) {
+    const mkeys = Object.keys(monkeys)
+    let index = 0
+    while (index < mkeys.length) {
+        const key = mkeys[index]
+        index += 1
+        const curr_monkey = monkeys[key]
+        const count = curr_monkey['inspect_count']
+        console.log(`Monkey ${key}: inspected items ${count} times`);
+    }
+
+}
+
+function get_new_level(worry, operand, factor) {
+    let new_worry = 0
+    if (factor == OLD) {
+        factor = worry
+    }
+
+    if (operand == PLUS) {
+        new_worry = worry + factor
+        return new_worry
+    } 
+    if (operand == MULTIPLY) {
+        new_worry = worry * factor
+        return new_worry
+    }
+
+    return new_worry
+}
+
+function assess_worry(worry, test, quotient) {
+
+    return (worry % quotient) == 0
+}
+
+function run_round(monkeys) {
+    let index = 0
+    const mkeys = Object.keys(monkeys)
+    while (index < mkeys.length) {
+        const key = mkeys[index]
+        index += 1
+
+        const curr_monkey = monkeys[key]
+        const items = curr_monkey['items']
+        let i = 0
+        while (i < items.length) {
+            const item = items[i]
+            i += 1
+
+            const count = curr_monkey['inspect_count']
+            curr_monkey['inspect_count'] = 1 + count
+
+            const operand = curr_monkey['operand']
+            const factor = curr_monkey['factor']
+            const inspect_worry = get_new_level(item, operand, factor)
+
+            const test = curr_monkey['test']
+            const quotient = curr_monkey['quotient']
+            const worried = assess_worry(inspect_worry, test, quotient)
+            const which_monkey = worried ? 'success_monkey' : 'fail_monkey'
+            const monkey_key = curr_monkey[which_monkey]
+
+            const next_monkey = monkeys[monkey_key]
+            next_monkey['items'].concat(inspect_worry)
+            monkeys[monkey_key] = next_monkey
+        }
+
+        curr_monkey['items'] = []
+        monkeys[key] = curr_monkey
+    } 
+
+    return monkeys
+}
+
+function calculate_monkey_business(monkeys) {
+    let monkey_business = 0
+
+    const counts = Object.keys(monkeys).map(key => {
+        return monkeys[key]['inspect_count']
+    });
+
+    const decreasing = counts.sort().reverse()
+    const first = decreasing[0]
+    const second = decreasing[1]
+    console.log(`1st[${first}] 2nd[${second}]`);
+
+    monkey_business = first * second
+
+    return monkey_business
+}
+
+function run_code(monkeys) {
+    //const rounds = 10000
+    //const rounds = 20
+    const rounds = 2
+    let round = 1
+
+    const range = [...Array(10).keys()]
+    const test = [1, 20].concat(range.map(x => (x+1)*1000))
+
+    while (round <= rounds) {
+        //monkeys = run_round(monkeys)
+        if (test.includes(round)) {
+            console.log("\nROUND: " + round);
+            display_monkeys(monkeys)
+        } else {
+            console.log("ROUND: " + round);
+        }
+        
+        round += 1
+    } 
+
+    return calculate_monkey_business(monkeys)
+}
 
 function assertEquals(expected, actual) {
     const output = (expected == actual) ? 
@@ -117,9 +230,9 @@ while (index < fileKeys.length) {
 
     const filename = `input_${aoc_day}${file}.txt`
     const data = parse_file(filename)
-    //const results = run_code(data)
-    //console.log("results: " + results)
-    //if (exp) {
-        //assertEquals(expected_result, results)
-    //}
+    const results = run_code(data)
+    console.log("results: " + results)
+    if (exp) {
+        assertEquals(exp, results)
+    }
 }
