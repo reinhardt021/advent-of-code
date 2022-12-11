06b3bfa4c931a24f01d74896820cf4f43388238d
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-11 19:25:26 +1300

    have the parsing working as needed and now to actually calculate the monkey business

--- 2022-1211/main_11a.rb
+++ 2022-1211/main_11a.rb
@@ -1,4 +1,11 @@
 class Main
+  MONKEY = 'Monkey'
+  STARTING = 'Starting'
+  OPERATION = 'Operation:'
+  TEST = 'Test:'
+  IF = 'If'
+  T = 'true:'
+  F = 'false:'
 
   def initialize(filename)
     file = File.open(filename)
@@ -7,6 +14,68 @@ class Main
   end
   
   def parse_monkeys(data)
+    monkeys = {}
+
+    curr_monkey = nil
+    data.each do |line|
+      parts = line.split(' ')
+      items = parts.map {|x|  "[#{x}]"}
+      puts items.join('')
+
+      line_type = parts[0]
+      if line_type == MONKEY
+        curr_monkey = parts[1].scan(/\d/).first.to_sym
+        monkeys[curr_monkey] = {
+          items: [],
+          operand: nil,
+          factor: nil,
+          test: nil,
+          quotient: nil,
+          success_monkey: nil,
+          fail_monkey: nil,
+        }
+      end
+
+      if curr_monkey == nil
+        next
+      end
+
+      if line_type == STARTING
+        items = line.scan(/\d+/)
+        #puts "items: #{items.map {|x| x.to_i}}"
+        monkeys[curr_monkey][:items] = items.map {|x| x.to_i}
+      end
+
+      if line_type == OPERATION
+        index_operand = 4
+        index_factor = 5
+        operand = parts[index_operand]
+        factor = parts[index_factor]
+        #puts "operand[#{operand}] factor[#{factor}]"
+        monkeys[curr_monkey][:operand] = operand
+        monkeys[curr_monkey][:factor] = factor
+      end
+
+      if line_type == TEST
+        index_test = 1
+        index_quotient = 3
+        test = parts[index_test]
+        quotient = parts[index_quotient]
+        monkeys[curr_monkey][:test] = test
+        monkeys[curr_monkey][:quotient] = quotient
+      end
+
+      if line_type == IF 
+        monkey = line.scan(/\d+/).first.to_i
+        #puts "MONKEY FOUND: " + monkey.to_s
+        success_or_fail = parts[1] == T ? :success_monkey : :fail_monkey
+        monkeys[curr_monkey][success_or_fail] = monkey
+      end
+
+    end
+
+    puts "monkeys: #{monkeys}"
+    return monkeys
   end
 
   def run
@@ -26,7 +95,7 @@ aoc_day = 11
 files = {
   #file => expected result
   'a' => 10605, # no expected result yet
-  'b' => nil, #10605,
+  #'b' => nil, #10605,
   #'c' => nil, #13140,
 }
 
