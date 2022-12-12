4ef9d7eaa1c838a62d83e59b2668404d4ed9041c
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-12 22:17:27 +1300

    have the day 12 started and the method for grabbing the fewest steps from an array of paths

---
+++ 2022-1212/main_12a.rb
@@ -0,0 +1,54 @@
+class Main
+  def initialize(filename)
+    file = File.open(filename)
+    lines = file.read.split("\n")
+    @height_map = lines.map {|line| line.split("") }
+  end
+  
+  def get_shortest_path(paths)
+    path_lengths = paths.map { |path| path.length }
+    fewest_steps = path_lengths.sort.first
+
+    return fewest_steps
+  end
+
+  def run
+
+    paths = [
+      ['0-0', '0-1', '1-1'],
+      ['0-0', '1-0', '1-1', '2-1', '2-2'],
+    ]
+
+    return get_shortest_path(paths)
+  end
+
+end
+
+
+def assertEquals(expected, actual)
+  if expected == actual
+    puts "PASS: exp[#{expected}] == act[#{actual}]"
+  else
+    puts "FAIL: exp[#{expected}] != act[#{actual}]"
+  end
+end
+
+aoc_day = 12
+files = {
+  #file => expected result
+  'a' => 31, 
+  #'b' => nil, # no expected result yet
+  #'c' => nil, #13140,
+}
+
+files.keys.each do |file|
+  expected_result = files[file]
+  filename = "input_#{aoc_day}#{file}.txt"
+  results = Main.new(filename).run
+  puts "results: " + results.to_s
+  if expected_result
+    assertEquals(expected_result, results)
+  end
+end
+
+
