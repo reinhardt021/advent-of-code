0df56c0ee3dcf1348d82fb05bd9f6aca7fa46ca9
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-12 23:15:19 +1300

    starting the checks for going to the next point

--- 2022-1212/main_12a.rb
+++ 2022-1212/main_12a.rb
@@ -2,17 +2,23 @@ class Main
   S_MARK = 'S'
   E_MARK = 'E'
 
-  def get_key(x, y)
-    return "#{y}-#{x}"
+  def get_point(x, y, height)
+    #return "#{y}-#{x}"
+
+    return {
+      x: x,
+      y: y,
+      height: height,
+    }
   end
   
   def parse_map(lines)
     height_map = {
       start: nil,
       end: nil,
-      elevations: [],
       row_count: lines.length,
       col_count: 0,
+      elevations: [],
     }
     y_index = 0
     while y_index < lines.length
@@ -25,14 +31,13 @@ class Main
       end_x = row.index(E_MARK)
 
       if start_x
-        puts "found starting y[#{y_index}] x[#{start_x}]"
-        height_map[:start] = get_key(start_x, y_index)
+        #puts "found starting y[#{y_index}] x[#{start_x}]"
+        height_map[:start] = get_point(start_x, y_index, 'a')
       end
 
       if end_x
-        puts "found ending y[#{y_index}] x[#{end_x}]"
-        height_map[:end] = get_key(end_x, y_index)
-        #height_map[:end] = "#{y_index}-#{end_x}"
+        #puts "found ending y[#{y_index}] x[#{end_x}]"
+        height_map[:end] = get_point(end_x, y_index, 'z')
       end
 
       height_map[:elevations] << row
@@ -56,8 +61,46 @@ class Main
     return fewest_steps
   end
 
+  def can_go_left(height_map, start_point)
+    #puts "a < b [#{'z' < 'r'}]" #// test to see chars compare works
+    if start_point[:x] == 0 
+      return false
+    end
+
+    new_x = start_point[:x] - 1
+    new_y = start_point[:y]
+    # get value at that next point
+    new_elevation = height_map[:elevations][new_y][new_x]
+    puts "y[#{new_y}] x[#{new_x}] e[#{new_elevation}]"
+
+    #if new_elevation
+
+  end
+
+  def get_paths(height_map, start_point)
+    # go from starting point
+    #   check if at edge && if not more than one higher
+    # check if can go left
+     go_left = can_go_left(height_map, start_point)
+    # check if can top
+    # check if can right
+    # check if can down
+    #   go deeper
+    #   get array of sub paths
+    #     filter if not contain endpoint
+    #     map to concat to curr point and return
+    #
+    # if none then return empty array
+    #
+    # break if reach end point then return last point in array
+    #
+    # if after 
+  end
+
   def run
-    # go throught the height map to find the 
+    # go throught the height map to find the the different paths
+    # recursively
+    paths = get_paths(@height_map, @height_map[:start])
 
     paths = [
       ['0-0', '0-1', '1-1'],
